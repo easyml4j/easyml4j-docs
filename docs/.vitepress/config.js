cde00eca8f767c05d536f4ba880eb7dc9df6d0df
chenxilin <635943647@qq.com>
2022-07-08 13:51:23 +0800

    feat: add mathjax config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,6 +2,10 @@ export default {
   title: "VitePress",
   lang: "en-US",
   description: "Vite & Vue powered static site generator.",
+  head: [
+    ["script", { src: "https://polyfill.io/v3/polyfill.min.js?features=es6" }],
+    ["script", { src: "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js" }]
+  ],
 
   // Theme related configurations.
   themeConfig: {
