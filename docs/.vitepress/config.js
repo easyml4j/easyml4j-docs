3910a3c998f7a6945d75705af7cd3ca177d79d7c
chenxilin <635943647@qq.com>
2022-07-10 11:10:48 +0800

    feat: add directory structure

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -25,7 +25,7 @@ export default {
   // Theme related configurations.
   themeConfig: {
     logo: "/logo.svg",
-    siteTitle: "Hello World",
+    siteTitle: "Easy ml for Java",
     // 导航栏是显示在页面顶部的导航栏。它包含站点标题、全局菜单链接等
     nav: [
       { text: "Guide", link: "/guide" },
@@ -51,23 +51,87 @@ export default {
       },
       {
         text: "线性模型",
+        collapsible: true,
         items: [
-          { text: "普通最小二乘法", link: "/md/线性模型/普通最小二乘法" },
-          { text: "岭回归", link: "/md/线性模型/岭回归" }
+          { text: "普通最小二乘法", link: "/ml/线性模型/普通最小二乘法" },
+          { text: "岭回归", link: "/ml/线性模型/岭回归" },
+          { text: "Lasso回归", link: "/ml/线性模型/Lasso回归" },
+          { text: "正交匹配追踪", link: "/ml/线性模型/正交匹配追踪" },
+          { text: "贝叶斯回归", link: "/ml/线性模型/贝叶斯回归" },
+          { text: "逻辑回归", link: "/ml/线性模型/逻辑回归" },
+          { text: "广义线性回归", link: "/ml/线性模型/广义线性回归" },
+          { text: "随机梯度下降", link: "/ml/线性模型/随机梯度下降" },
+          { text: "感知器", link: "/ml/线性模型/感知器" },
+          { text: "被动攻击算法", link: "/ml/线性模型/被动攻击算法" },
+          { text: "鲁棒性回归", link: "/ml/线性模型/鲁棒性回归" },
+          { text: "分位数回归", link: "/ml/线性模型/分位数回归" },
+          { text: "多项式回归", link: "/ml/线性模型/多项式回归" }
+        ]
+      },
+      {
+        text: "贝叶斯",
+        collapsible: true,
+        items: [{ text: "朴素贝叶斯", link: "/ml/贝叶斯/朴素贝叶斯" }]
+      },
+      {
+        text: "支持向量机",
+        collapsible: true,
+        items: [
+          { text: "分类", link: "/ml/支持向量机/分类" },
+          { text: "回归", link: "/ml/支持向量机/回归" },
+          { text: "密度估计", link: "/ml/支持向量机/密度估计" },
+          { text: "异常检测", link: "/ml/支持向量机/异常检测" }
         ]
       },
       {
         text: "树模型",
+        collapsible: true,
         items: [
-          { text: "决策树", link: "/index" },
-          { text: "梯度提升树", link: "/getting-started" }
+          { text: "决策树", link: "/ml/树模型/决策树" },
+          { text: "梯度提升树", link: "/ml/树模型/梯度提升树" },
+          { text: "最近邻", link: "/ml/树模型/最近邻" }
         ]
       },
       {
         text: "集成模型",
+        collapsible: true,
         items: [
-          { text: "普通最小二乘法", link: "/index" },
-          { text: "岭回归", link: "/getting-started" }
+          { text: "随机森林", link: "/ml/集成模型/随机森林" },
+          { text: "Adaboost", link: "/ml/集成模型/Adaboost" }
+        ]
+      },
+      {
+        text: "聚类",
+        collapsible: true,
+        items: [
+          { text: "KMeans聚类", link: "/ml/聚类/KMeans聚类" },
+          { text: "均值漂移", link: "/ml/聚类/均值漂移" },
+          { text: "层次聚类", link: "/ml/聚类/层次聚类" },
+          { text: "Affinity Propagation", link: "/ml/聚类/AffinityPropagation" },
+          { text: "谱聚类", link: "/ml/聚类/谱聚类" },
+          { text: "OPTICS", link: "/ml/聚类/OPTICS" },
+          { text: "DBSCAN", link: "/ml/聚类/DBSCAN" },
+          { text: "BIRCH", link: "/ml/聚类/BIRCH" }
+        ]
+      },
+      {
+        text: "模型选择与评估",
+        collapsible: true,
+        items: [
+          { text: "模型选择", link: "/ml/模型选择与评估/模型选择" },
+          { text: "回归指标", link: "/ml/模型选择与评估/回归" },
+          { text: "分类指标", link: "/ml/模型选择与评估/分类" },
+          { text: "聚类指标", link: "/ml/模型选择与评估/聚类" }
+        ]
+      },
+      {
+        text: "特征处理",
+        collapsible: true,
+        items: [
+          { text: "缺失值处理", link: "/ml/特征处理/缺失值处理" },
+          { text: "异常值处理", link: "/ml/特征处理/异常值处理" },
+          { text: "特征选择", link: "/ml/特征处理/特征选择" },
+          { text: "特征转换", link: "/ml/特征处理/特征转换" }
         ]
       }
     ]
